a8f172a1fe0acf6f853c703389e357e3a770542a
Salada-De-Flutter <fluttered001@gmail.com>
2025-09-24 08:47:38 -0300

    Adição do database

--- app.py
+++ app.py
@@ -1,21 +1,38 @@
-from flask import Flask
-import psycopg2
+from flask import Flask, render_template, request
 from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
 import os
 
 load_dotenv()
 
-def get_db():
-    return psycopg2.connect(os.getenv('DATABASE_URL'))
+database_url = os.getenv('DATABASE_URL')
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+db = SQLAlchemy(app)
 
 try:
-    conn = get_db()
-    print('Database conectou')
-    conn.close()
-except:
-    print('Erro na conexão com o database')
+    with app.app_context():
+        db.engine.connect
+        print("Banco de dados conectado")
+except Exception as e:
+    print(f"Algo deu errado {e}")
 
-app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/mandar', methods = ['POST'])
+def mandar():
+    usuario = request.form['usuario']
+    senha = request.form['senha']
+    novo_user = Usuario(senha = senha, usuario = usuario)
+    return f'O usuario é: {usuario} e a senha é {senha}'
+
+class Usuario(db.Model):
+    __tablename__ = 'teste'
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.Text)
+    senha = db.Column(db.Text)
 
 if __name__ == '__main__':
     app.run(debug=True)
