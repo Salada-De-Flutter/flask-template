662db8693d8b08c9e30fe9c220cbe69f17b11b10
Salada-De-Flutter <fluttered001@gmail.com>
2025-09-24 11:56:40 -0300

    Consegui resgatar algo do database

--- app.py
+++ app.py
@@ -1,36 +1,52 @@
 from flask import Flask, render_template, request
-from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
-database_url = os.getenv('DATABASE_URL')
 app = Flask(__name__)
+database_url = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 db = SQLAlchemy(app)
+import uuid
 
 try:
     with app.app_context():
-        db.engine.connect
-        print("Banco de dados conectado")
+        db.engine.connect()
+        print("DATABASE CONECTADO")
 except Exception as e:
-    print(f"Algo deu errado {e}")
+    print(f"DATABASE NÃO CONECTADO: {e}")
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route('/mandar', methods = ['POST'])
+@app.route('/mandar', methods=['POST'])
 def mandar():
-    usuario = request.form['usuario']
+    nome = request.form['nome']
     senha = request.form['senha']
-    novo_user = Usuario(senha = senha, usuario = usuario)
-    return f'O usuario é: {usuario} e a senha é {senha}'
-
-class Usuario(db.Model):
-    __tablename__ = 'teste'
-    id = db.Column(db.Integer, primary_key=True)
+    novo_user = Users(nome = nome, senha = senha, id = uuid.uuid4())
+    db.session.add(novo_user)
+    db.session.commit
+    try:
+        return render_template('login.html')
+    except Exception as e:
+        return f"Usuario não adiconado: {e}"
+    
+@app.route('/login', methods=['POST'])
+def logar():
+    nome = request.form['nome']
+    senha = request.form['senha']
+    usuario = Users.query.filter_by(nome=nome).first()
+    if senha == usuario.senha and nome == usuario.nome:
+        return f"id do usuario: {usuario.id}"
+    else:
+        return "Usuario não encontrado"
+        
+class Users(db.Model):
+    __tablename__ = 'teste2'
+    id = db.Column(db.Text, primary_key=True, not_null=True)
     nome = db.Column(db.Text)
     senha = db.Column(db.Text)
 
